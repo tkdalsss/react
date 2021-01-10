754adcf74fd6df892886246cee58e206e402bf9e
tkdalsss <lsm991229@naver.com>
2021-01-10 15:20:10 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
-import React from "react";
 import "./style.css";
+import React from "react";
+import PropTypes from "prop-types";
 
-function Movie({fav}) {
+const movieLike = [
+  {
+    id: 1,
+    name: "Mission Impossible: FALL OUT",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/f/ff/MI_%E2%80%93_Fallout.jpg",
+    rating: 4.5
+  },
+  {
+    id: 2,
+    name: "Frozen2",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/4/4f/Frozen_2_poster.jpg/220px-Frozen_2_poster.jpg",
+    rating: 4.3
+  },
+  {
+    id: 3,
+    name: "Avengers: END GAME",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg",
+    rating: 5
+  },
+  {
+    id: 4,
+    name: "SpiderMan: Far From Home",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg",
+    rating: 4.0
+  }
+];
+
+function Movie({ name, picture, rating }) {
   return (
-    <p>I like {fav}</p>
+    <div>
+      <h3>{name}</h3>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
   );
+}
+
+// Describe something that I want to get
+Movie.propTypes = {
+
 }
 
 export default function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Movie fav="Mission Impossible" />
-      <Movie fav="Frozen"/>
-      <Movie fav="Avengers"/>
+      {movieLike.map(ticket => (
+        <Movie key={ticket.id} name={ticket.name} picture={ticket.image}
+        rating={ticket.rating} />
+      ))}
     </div>
   );
 }

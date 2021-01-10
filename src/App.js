d16b14be381324b67708d319772eb3318b289b38
tkdalsss <lsm991229@naver.com>
2021-01-10 11:06:40 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./style.css";
 
+function Movie({fav}) {
+  return (
+    <p>I like {fav}</p>
+  );
+}
+
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello World!</h1>
+      <Movie fav="Mission Impossible" />
+      <Movie fav="Frozen"/>
+      <Movie fav="Avengers"/>
     </div>
   );
 }
